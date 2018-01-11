0977171e7c1cfe707120a02608f15ec322f39833
Aelitta Ezugbaya <aelitta.ezugbaya@nordpoolgroup.com>
2018-01-11 10:10:50 +0200

    created module and ctrl with typescript

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,28 @@
 /// <reference path="../vendor/types/angularjs/angular.d.ts"/>
 
-(function(){
-    var app = angular.module('shopping-list',[]);
+// (function(){
+//     var app = angular.module('shopping-list',[]);
 
-    app.controller('TitleController',function(){
-        this.title='title';
-    })
-})();
+//     app.controller('TitleController',function(){
+//         this.title='title';
+//     })
+// })();
+
+var app = angular.module('shopping-list',[]);
+
+interface IItem{
+    name:string;
+    done:boolean; 
+};
+
+class ItemCtrl implements IItem{
+    name:string;
+    done:boolean; 
+    
+    constructor() {
+         this.name = "Milk";
+         this.done = false;
+    }
+}
+
+app.controller('ItemCtrl',ItemCtrl);
