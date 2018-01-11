1c5e57cc0b2e6551541a9f23391201efabff8cb5
Aelitta Ezugbaya <aelitta.ezugbaya@nordpoolgroup.com>
2018-01-11 09:47:38 +0200

    write my module in ts file and compiled/ add reference to angular ts in app js

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+/// <reference path="../vendor/types/angularjs/angular.d.ts"/>
+
+(function(){
+    var app = angular.module('shopping-list',[]);
+
+    app.controller('TitleController',function(){
+        this.title='title';
+    })
+})();
